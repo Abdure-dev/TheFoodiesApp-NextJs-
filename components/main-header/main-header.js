52ecb91754a09e5fa010dcb7d0070620d1c51712
Abdure-dev <abdurahmanethiopia@gmail.com>
2025-07-29 16:03:02 -0500

    inital commit

---
+++ components/main-header/main-header.js
@@ -0,0 +1,31 @@
+import Link from "next/link";
+import Image from "next/image";
+
+import MainHeaderBackground from "./main-header-background";
+import logoImg from "@/assets/logo.png";
+import syles from "./header-style.module.css";
+export default function MainHeader() {
+  return (
+    <>
+      <MainHeaderBackground />
+      <header className={syles.header}>
+        <Link className={syles.logo} href="/">
+          <Image src={logoImg} alt="A plate with food on it" priority />
+          NextLevel Food
+        </Link>
+        <nav className={syles.nav}>
+          <ul>
+            <li>
+              {" "}
+              <Link href="/meals">Browse Meals</Link>
+            </li>
+            <li>
+              {" "}
+              <Link href="/community">Browse Meals</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
+}
